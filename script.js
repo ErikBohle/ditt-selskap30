00d4673bb6d7180c8570093e8b237cdf569def21
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-10-14 06:43:02 +0200

    added slider/carousel

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ function toggleNavigation() {
       menu.className = "navbar";
     }
   }
+// slider
+var slideIndex = 0;
+carousel();
 
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); //(carousel, x)  X Changes image every 4 seconds
+}
