baed4340bfd7e3ee57b41e8494cc2b1217087fd0
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-10-19 20:08:58 +0200

    Fixed map API

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-/*TOGGLE RESPONSIVE NAVIGATION*/
-
-function toggleNavigation() {
-  let menu = document.getElementById("nav");
-  if (menu.className === "navbar") {
-    menu.className += " responsive";
-  } else {
-    menu.className = "navbar";
-  }
-}
-// slider
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {
-    slideIndex = 1;
-  }
-  x[slideIndex - 1].style.display = "block";
-  setTimeout(carousel, 4000); //(carousel, x)  X Changes image every 4 seconds
-}
 
 /** Geolocation API */
 
@@ -76,3 +49,30 @@ function showMap(position) {
 
 // One-shot position request.
 navigator.geolocation.getCurrentPosition(showMap);
+/*TOGGLE RESPONSIVE NAVIGATION*/
+
+function toggleNavigation() {
+  let menu = document.getElementById("nav");
+  if (menu.className === "navbar") {
+    menu.className += " responsive";
+  } else {
+    menu.className = "navbar";
+  }
+}
+// slider
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 4000); //(carousel, x)  X Changes image every 4 seconds
+}
